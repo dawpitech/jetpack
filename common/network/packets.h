ee8d8b6357fd1a4c1e53b427f70484a97dc41e23
Antoine <me@antoinebellanger.fr>
2025-04-03 08:40:53 +0200

    feat: player update packet

--- common/network/packets.h
+++ common/network/packets.h
@@ -14,14 +14,27 @@
         #include <stdint.h>
     #endif
 
+    #define PACKET_BUFFER_SIZE 1024
+
+typedef float   float32_t;
+typedef double  float64_t;
+
 typedef enum packet_type_e {
     INVALID,
     ACKNOWLEDGE,
     HELLO,
+    PLAYER_UPDATE,
 } packet_type_t;
 
 typedef struct packet_generic_s {
     packet_type_t type;
 } packet_generic_t;
+
+typedef struct packet_player_update_s {
+    packet_type_t type;
+    float32_t x;
+    float32_t y;
+    int on_the_floor;
+} packet_player_update_t;
 
 #endif //PACKETS_H
